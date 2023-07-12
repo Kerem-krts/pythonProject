1ae48a1041b9ba219596d90853cfd117866a878e
KEREM <kerem.karatas.5665@gmail.com>
2023-07-12 10:45:42 +0300

    create repo

--- staj ödevi 1.py
+++ staj ödevi 1.py
@@ -4,7 +4,7 @@ path = r"C:\Users\KEREM\Desktop\iris\iris.data" #dataset yolu
 csv_data = pd.read_csv(path, delimiter=",",header=None) #dataset okuma
 
 exel_path = r"C:\Users\KEREM\Desktop\iris\iris2.xlsx" #exel yolu
-smp_csv_data = csv_data.sample(frac=1) #karıştırms
+smp_csv_data = csv_data.sample(frac=1) #karıştırma
 
 smp_csv_data[4] = smp_csv_data[4].replace("Iris-setosa", "1")  #değer değiştirme
 smp_csv_data[4] = smp_csv_data[4].replace("Iris-virginica", "2")
