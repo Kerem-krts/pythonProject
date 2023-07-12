4fa11a48b51532221a3cd30af29195ca16d710f6
KEREM <kerem.karatas.5665@gmail.com>
2023-07-12 10:48:16 +0300

    editing

--- staj ödevi 1.py
+++ staj ödevi 1.py
@@ -3,7 +3,7 @@ path = r"C:\Users\KEREM\Desktop\iris\iris.data" #dataset yolu
 
 csv_data = pd.read_csv(path, delimiter=",",header=None) #dataset okuma
 
-exel_path = r"C:\Users\KEREM\Desktop\iris\iris2.xlsx" #exel yolu
+exel_path = r"C:\Users\KEREM\Desktop\iris\iris2.xlsx" #excel yolu
 smp_csv_data = csv_data.sample(frac=1) #karıştırma
 
 smp_csv_data[4] = smp_csv_data[4].replace("Iris-setosa", "1")  #değer değiştirme
